34a80a31456a8bad487f83073780f3538608bf40
Jesse Parent <jesseparent@gmail.com>
2020-08-30 15:45:12 -0600

    Code cleanup and refactor wrote all HTML for columns and rows in Javascript fixed some display bugs removed console logs added README and Screenshots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,46 @@
 // Get the date and time right now
-let today = moment("12PM", "hA"); // for testing
-//let today = moment();
+let today = moment();
 
-// Time Slots for Each row in calendar
-let schedule = JSON.parse(localStorage.getItem('schedule')) 
+// Time Slots for Each row in calendar - use pseudo associative array
+let schedule = JSON.parse(localStorage.getItem('schedule')) // Check local storage for values
+  // If there is nothing in local storage, create an empty schedule
   || [
-  { "9AM": "" },
-  { "10AM": "" },
-  { "11AM": "" },
-  { "12PM": "" },
-  { "1PM": "" },
-  { "2PM": "" },
-  { "3PM": "" },
-  { "4PM": "" },
-  { "5PM": "" }
-];
+    { "9AM": "" },
+    { "10AM": "" },
+    { "11AM": "" },
+    { "12PM": "" },
+    { "1PM": "" },
+    { "2PM": "" },
+    { "3PM": "" },
+    { "4PM": "" },
+    { "5PM": "" }
+  ];
 
-// Timer to check if colors on schedule need to be updated due to hour change
+// Timers to check if colors on schedule need to be updated due to hour change
+let initialCheckTimeout;
 let updateInterval;
 
-// Format Date to Day-Of-Week, Month-Name Day-Suffixed (e.g. "Thursday, January 7th") and display it at top of page
-$('#currentDay').text(today.format("dddd, MMMM Do"));
-
 // Render the schedule with stored tasks
-let renderSchedule = function() {
+let renderSchedule = function () {
+  // Format Date to Day-Of-Week, Month-Name Day-Suffixed (e.g. "Thursday, January 7th") and display it at top of HTML page
+  $('#currentDay').text(today.format("dddd, MMMM Do"));
+
+  // Write out the schedule
+  let scheduleContainer = $('.container');
+
   for (let i = 0; i < schedule.length; i++) {
-    let currentKey = Object.keys(schedule[i])[0]; // Get the time of the time slot to evaluate
-    let textEntry = $("#" + currentKey); // Find the class of the textarea to change
-    textEntry.val(schedule[i][currentKey]); // Change the text entry to match the event for that time
+    let currentKey = Object.keys(schedule[i])[0];
+    let currentValue = schedule[i][currentKey];
+    let rowText = ''; // Set up row text
+
+    // Create row for this time slot
+    rowText += '<div class="row time-block">\n';
+    rowText += '  <div class="col-1 hour m-0 p-0">' + currentKey + '</div>\n';
+    rowText += '  <div class="col-10 description m-0 p-0"><textarea id="' + currentKey + '" class="w-100 h-100">' + currentValue + '</textarea></div>\n';
+    rowText += '  <div class="col-1 m-0 p-0"><button class="saveBtn w-100 h-100"><i class="fas fa-save"></i></button></div>\n';
+    rowText += '</div>\n';
+
+    scheduleContainer.append(rowText);
   }
 
   // Change the colors of each time slot based on the current time
@@ -36,13 +49,15 @@ let renderSchedule = function() {
 
 // Change the colors of the timeslots in the Workday schedule
 let changeColors = function () {
-  console.log("change colors");
+  // Load time right now to see if we need to change colors on backgrounds
+  today = moment();
+  
   for (let i = 0; i < schedule.length; i++) {
     let timeToCheck = Object.keys(schedule[i])[0]; // Get the time of the time slot to evaluate
     let textEntry = $("#" + timeToCheck); // Find the class of the textarea to change
 
-    let currentHour = moment(today, "hA"); // The current hour on the user's clock
-    let rowHour = moment(timeToCheck, "hA"); // The hour of the time slot to evaluate
+    let currentHour = moment(today.format("hA"), "hA"); // The current hour on the user's clock and store as string
+    let rowHour = moment(timeToCheck, "hA"); // The hour of the time slot to evaluate and store as string
 
     // Clear current classes that style the background color based on time
     textEntry.removeClass("past present future");
@@ -60,26 +75,31 @@ let changeColors = function () {
   }
 };
 
-// After first time gets me close to teh start of the hour, check on an hourly basis if the colors on teh scheduler need to change
-let setupHourlyCheck = function() {
-  changeColors();  // Change the background colors of hourly time slots based on teh current hour
+// After first timer gets me close to the start of the hour, check on an hourly basis if the colors on the scheduler need to change
+let setupHourlyCheck = function () {
+  // Change the background colors of hourly time slots based on the current hour
+  changeColors();
 
-  let anHour = 60 * 60 * 1000; // 60 minutes * 60 seconds * 1000 milliseconds
-  
+  // Get the milliseconds in an hour: 60 minutes * 60 seconds * 1000 milliseconds
+  let anHour = 60 * 60 * 1000; 
+
+  // Change the background colors of hourly time slots every hour from now on
+  clearInterval(updateInterval);
   updateInterval = setInterval(changeColors, anHour);
-}
+};
 
-// Save the entry on teh current time to in memory and local storage
-$(".saveBtn").click(function()
-{
-  let key = $(this).attr("data-txt-id");
+// Render the schedule
+renderSchedule();
+
+// Save the entry on the current time to in memory and local storage
+$(".saveBtn").click(function () {
+  let key = $(this).closest(".time-block").children(".hour").text();
   let value = $(this).closest(".time-block").children(".description").children().val();
-
+  
   // Find and change the current time entry in the array
   for (let i = 0; i < schedule.length; i++) {
     let currentKey = Object.keys(schedule[i])[0];
-    if (currentKey === key)
-    {
+    if (currentKey === key) {
       schedule[i][currentKey] = value;
       break;
     }
@@ -89,12 +109,9 @@ $(".saveBtn").click(function()
   localStorage.setItem('schedule', JSON.stringify(schedule));
 });
 
-// Render the schedule
-renderSchedule();
-
 // Set a timer based on how close the next hour is and then set an hourly interval timer
 let timeToNextHour = (60 - moment().format("m") + 1) * 60 * 1000; // Milliseconds to the next hour, plus 1 minute extra to account for overage due to seconds
 
-let initialCheckTimeout = setTimeout(setupHourlyCheck, timeToNextHour);
-
-
+// Clear timer and then set it
+clearTimeout(initialCheckTimeout);
+initialCheckTimeout = setTimeout(setupHourlyCheck, timeToNextHour);
